6532edaa4c3f06abe70261494f5dead5ea145105
MarkDekker1 <m.m.dekker@outlook.com>
2016-05-16 18:34:01 +0200

    Final

--- DOP1/DataPlotting.py
+++ DOP1/DataPlotting.py
@@ -7,7 +7,15 @@ import matplotlib.cm as cm
 from mpl_toolkits.basemap import Basemap
 from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d import axes3d
+from matplotlib.colors import BoundaryNorm
+from matplotlib.colors import LogNorm
+import matplotlib.colors as colors
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d import axes3d
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import scipy
+import matplotlib.colors as colors
 
 #Preambule plots
 Lon_0 = 0.
@@ -18,8 +26,8 @@ lon, lat = np.meshgrid(Lon2, Lat2)
 #%% Plot Streamfunction
 
 # Limits
-lim1=-1e4
-lim2=1e4
+lim1=-1e5
+lim2=1e5
 
 #Actual plot
 plt.figure(num=None, figsize=(10,6),dpi=150, facecolor='w', edgecolor='k')
@@ -29,32 +37,35 @@ m = Basemap(llcrnrlon=-90,llcrnrlat=10,urcrnrlon=30,urcrnrlat=70,
             lat_ts=40,lat_0=Lat_0,lon_0=Lon_0)
 xi, yi = m(lon, lat)
 
-v = np.linspace(lim1,lim2,101,endpoint=True)
+v = np.linspace(lim1,lim2,200,endpoint=True)
 v2 = np.linspace(lim2,10000000,4,endpoint=True)
 v3 = np.linspace(-100000000,lim1,4,endpoint=True)
+vlog=[lim1,lim1/5,lim1/10,lim1/100,0,lim2/100,lim2/10,lim2/5,lim2]
 
-m.contour(xi,yi,Stream,levels=([0]),colors='grey')
+bnorm = BoundaryNorm(vlog, ncolors=250, clip=False)
+
+m.contour(xi,yi,Stream,levels=([-4000,0,20000]),colors='grey')
 m.contourf(xi,yi,Stream,levels=v2,colors='maroon')
 m.contourf(xi,yi,Stream,levels=v3,colors='navy')
-Colors = m.contourf(xi,yi,Stream,levels=v,cmap=plt.cm.jet)
+
+Colors = m.contourf(xi,yi,Stream,levels=v,cmap=plt.cm.jet,norm=colors.SymLogNorm(linthresh=500, linscale=1e-10,vmin=-200000, vmax=200000,clip=False))
 
 r=5
-m.quiver(xi[::r,::r],yi[::r,::r],VelocityU[::r,::r]/VelocityT[::r,::r],VelocityV[::r,::r]/VelocityT[::r,::r],color='k',headlength=7,scale=40)
-#m.streamplot(xi,yi,VelocityU,VelocityV,color=VelocityT,cmap=cm.cool,linewidth=2,arrowstyle='->',arrowsize=1.5)
+#m.quiver(xi[::r,::r],yi[::r,::r],VelocityU[::r,::r]/VelocityT[::r,::r],VelocityV[::r,::r]/VelocityT[::r,::r],color='k',headlength=7,scale=40)
         
 m.fillcontinents(color='grey')
 m.drawparallels(np.arange(10., 71., 30), labels=[1,0,0,0], fontsize=12)
 m.drawmeridians(np.arange(-180., 180., 30), labels=[0,0,0,1], fontsize=12)
 
 m.drawcoastlines()
-cbar = m.colorbar(Colors, extend='both', location='bottom',pad="10%")
+cbar = m.colorbar(Colors,spacing='log',ticks=[-100000,-10000,-1000,0,1000,10000,100000], location='bottom', pad="10%")
 plt.show()
 
 #%% Plot Meridional Velocity
 
 # Limits
-lim1=-1e-4
-lim2=1e-4
+lim1=-2e-3
+lim2=2e-3
 
 # Actual plot
 plt.figure(num=None, figsize=(10,6),dpi=150, facecolor='w', edgecolor='k')
@@ -64,13 +75,14 @@ m = Basemap(llcrnrlon=-90,llcrnrlat=10,urcrnrlon=30,urcrnrlat=70,
             lat_ts=40,lat_0=Lat_0,lon_0=Lon_0)
 xi, yi = m(lon, lat)
 
-v = np.linspace(lim1,lim2,21,endpoint=True)
+v = np.linspace(lim1,lim2,200,endpoint=True)
 v2 = np.linspace(lim2,10000000,4,endpoint=True)
 v3 = np.linspace(-100000000,lim1,4,endpoint=True)
 
+m.contour(xi,yi,VelocityV,levels=([0]),colors='grey')
 m.contourf(xi,yi,VelocityV,levels=v2,colors='DarkRed')
 m.contourf(xi,yi,VelocityV,levels=v3,colors='DarkBlue')
-Colors = m.contourf(xi,yi,VelocityV,levels=v,cmap=plt.cm.jet)
+Colors = m.contourf(xi,yi,VelocityV,levels=v,cmap=plt.cm.jet,norm=colors.SymLogNorm(linthresh=0.0004, linscale=1000,vmin=lim1,vmax=lim2))
         
 m.fillcontinents(color='grey')
 m.drawparallels(np.arange(-90., 91., 30), labels=[1,0,0,0], fontsize=12)
@@ -78,14 +90,14 @@ m.drawmeridians(np.arange(-180., 180., 30), labels=[0,0,0,1], fontsize=12)
 
 m.drawcoastlines()
 
-cbar = m.colorbar(Colors, extend='both', location='bottom',pad="10%")
+cbar = m.colorbar(Colors, spacing= 'log',ticks=[lim1,lim1/10,0,lim2/10,lim2], location='bottom',pad="10%")
 plt.show()
 
 #%% Plot Zonal Velocity
 
 # Limits
-lim1=-5e-2
-lim2=5e-2
+lim1=-8e-2
+lim2=8e-2
 
 # Actual plot
 plt.figure(num=None, figsize=(10,6),dpi=150, facecolor='w', edgecolor='k')
@@ -95,13 +107,14 @@ m = Basemap(llcrnrlon=-90,llcrnrlat=10,urcrnrlon=30,urcrnrlat=70,
             lat_ts=40,lat_0=Lat_0,lon_0=Lon_0)
 xi, yi = m(lon, lat)
 
-v = np.linspace(lim1,lim2,21,endpoint=True)
+v = np.linspace(lim1,lim2,100,endpoint=True)
 v2 = np.linspace(lim2,10000000,4,endpoint=True)
 v3 = np.linspace(-100000000,lim1,4,endpoint=True)
 
+m.contour(xi,yi,VelocityU,levels=([0]),colors='grey')
 m.contourf(xi,yi,VelocityU,levels=v2,colors='DarkRed')
 m.contourf(xi,yi,VelocityU,levels=v3,colors='DarkBlue')
-Colors = m.contourf(xi,yi,VelocityU,levels=v,cmap=plt.cm.jet)
+Colors = m.contourf(xi,yi,VelocityU,levels=v,cmap=plt.cm.jet,norm=colors.SymLogNorm(linthresh=0.05, linscale=10,vmin=lim1,vmax=lim2))
         
 m.fillcontinents(color='grey')
 m.drawparallels(np.arange(-90., 91., 30), labels=[1,0,0,0], fontsize=12)
@@ -109,7 +122,7 @@ m.drawmeridians(np.arange(-180., 180., 30), labels=[0,0,0,1], fontsize=12)
 
 m.drawcoastlines()
 
-cbar = m.colorbar(Colors, extend='both', location='bottom',pad="10%")
+cbar = m.colorbar(Colors, spacing= 'log',ticks=[lim1,lim1/10,0,lim2/10,lim2],extend='both', location='bottom',pad="10%")
 plt.show()
 
 #%% Plot Total Velocity
--- DOP1/DataProcessing.py
+++ DOP1/DataProcessing.py
@@ -9,6 +9,29 @@ from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d import axes3d
 import scipy
 
+
+#Defining right boundary of the North Atlantic for discrete integration
+Bvec=[]
+for i in range(0,181):
+    Boundary=0.
+    k=150
+    if i<30:
+	     k=180
+    while Dtot[i][k]>0 and k<359:
+        Boundary=k
+        k=k+1      
+    Bvec.append(Boundary)
+Bvec[51]=170.
+Bvec[54]=172.
+
+def RM(x, N):
+    return np.convolve(x, np.ones((N,))/N)[(N-1):]
+
+#Smoothen bathymetry
+#Dtot2=Dtot
+#for i in range(0,len(Dtot[0])):
+#	Dtot2[:,i]=RM(Dtot2[:,i],5)
+
 #Constantem
 Cd=0.0015
 rhoa=1.225
@@ -23,20 +46,6 @@ Vstress=rhoa*Cd*V**2*V/abs(V)
 def xlen(latitude):
     return abs(6400000*2*np.pi/360*sin((latitude-90)/360.*2.*np.pi))
 
-#Defining right boundary of the North Atlantic for discrete integration
-Bvec=[]
-for i in range(0,181):
-    Boundary=0.
-    k=150
-    if i<30:
-	 k=180
-    while Dtot[i][k]>0 and k<359:
-        Boundary=k
-        k=k+1      
-    Bvec.append(Boundary)
-Bvec[51]=170.
-Bvec[54]=172.
-
 #Calculate Curl of stress and stream function
 Curlstressvec=[]
 Stream=np.zeros(shape=(181,360))
@@ -47,10 +56,10 @@ for i in range(0,181):
     xlength=xlen(phi)
     Curlstressvec.append(gradient(Vstress[i,:])/(xlength)-np.transpose(gradient(np.transpose(Ustress)[:,i]))/ylength)
     for j in range(0,360):
-	a=Curlstressvec[i]
-	Stream[i,j]=sum(a[j:Bvec[i]])*(Bvec[i]-j)*xlength
+		a=Curlstressvec[i]
+		Stream[i,j]=sum(a[j:Bvec[i]])*(Bvec[i]-j)*xlength
         
-Stream=Stream/(beta0*rhow*Dtot)
+Stream=-Stream/(beta0*rhow*Dtot)
 
 #Calculate Meridional and Zonal velocity
 VelocityV=np.array(Curlstressvec)/(beta0*rhow*Dtot)
@@ -59,18 +68,8 @@ VelocityU=[]
 for i in range(0,360):
 	 VelocityU.append(-gradient(np.transpose(Stream)[i])/ylength)
 	
-VelocityU=np.transpose(VelocityU)/10
+VelocityU=np.transpose(VelocityU)
 
 VelocityT=np.sqrt(VelocityU**2+VelocityV**2)
 
-#%%
-for i in range(0,len(VelocityV)):
-	for j in range(0,len(VelocityV[1])):
-		if abs(VelocityV[i,j])>5:
-			VelocityV[i,j]=0
-		if abs(VelocityU[i,j])>5:
-			VelocityU[i,j]=0
-		if j>210 or j<90 or i>80 or j<20:
-			VelocityV[i,j]=0
-			VelocityU[i,j]=0
-			
+#Integration constant in U
